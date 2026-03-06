d1425224ef7015a1cfcee9dde264f56d39be0c87
Raghav Bandreddi <raghavbandreddy@gmail.com>
2026-03-06 13:26:24 +0530

    Added vision app

--- AI_Dev_Projects/vision_app.py
+++ AI_Dev_Projects/vision_app.py
@@ -1,4 +1,4 @@
-import gradio as gd
+import gradio as gr
 from PIL import  Image
 from transformers import AutoProcessor,BlipForConditionalGeneration
 
@@ -12,3 +12,20 @@ def caption_image(input_image):
     inputs = processor(images=raw_image,return_tensors="pt")
 
     out = model.generate(**inputs,max_length=50)
+
+    caption = processor.decode(out[0],skip_special_tokens=True)
+
+    return caption
+
+iface = gr.Interface(
+    fn=caption_image,
+    inputs=gr.Image(type="pil"),
+    outputs="text",
+    title="Raghav's Local AI vision App",
+    description="Drag and drop any image here,and offline BLIP model tell you what is sees!"
+)
+
+if __name__ == "__main__":
+    iface.launch()
+
+
